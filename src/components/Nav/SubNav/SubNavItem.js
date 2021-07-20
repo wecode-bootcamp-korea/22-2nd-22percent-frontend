1090ff8a26a9d1a48f531af669f8ee794f44d79a
kylee <anachrosh@gmail.com>
2021-07-20 21:26:32 +0900

    Modify: 리뷰 사항 수정

--- src/components/Nav/SubNav/SubNavItem.js
+++ src/components/Nav/SubNav/SubNavItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-function SubNavItem(props) {
+function SubNavItem({ children }) {
+  const location = useLocation();
+
   return (
     <>
-      {props.children.map((li, i) => (
-        <Item key={i}>{li}</Item>
+      {children.map(li => (
+        <Link to={li.path} key={li.id}>
+          <Item isSelected={li.path === location.pathname}>{li.name}</Item>
+        </Link>
       ))}
     </>
   );
@@ -14,13 +20,25 @@ function SubNavItem(props) {
 export default SubNavItem;
 
 const Item = styled.li`
+  position: relative;
   margin: 0 15px 0;
-  padding: 20px 30px;
-  color: ${({ theme }) => theme.colorTitle};
+  padding: 20px 5px;
+  margin: 0 25px;
+  color: ${({ theme, isSelected }) =>
+    isSelected ? 'black' : theme.colorTitle};
   transition: color 0.3s ease-out;
   cursor: pointer;
 
   &:hover {
     color: black;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-bottom: ${({ isSelected }) => isSelected && '3px solid black'};
   }
 `;
---
+++ src/components/Nav/SubNav/navTabList.js
@@ -0,0 +1,27 @@
+const DEALS = [
+  {
+    id: 'deals_0',
+    name: '부동산',
+    path: '/deals/real-estate',
+  },
+  {
+    id: 'deals_1',
+    name: '개인신용',
+    path: '/deals/individual',
+  },
+];
+
+const MYPAGE = [
+  {
+    id: 'my_0',
+    name: '투자요약',
+    path: '/my/investor/overview',
+  },
+  {
+    id: 'my_1',
+    name: '투자내역',
+    path: '/my/investor/investments',
+  },
+];
+
+export { DEALS, MYPAGE };
