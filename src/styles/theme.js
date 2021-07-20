a33b4af5bed160b90d14e16c598a7849e00282fc
kylee <anachrosh@gmail.com>
2021-07-20 18:46:04 +0900

    Modify: 스타일 초기 설정 수정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,10 +4,8 @@ const theme = {
   colorDesc: '#9CA5AD',
   colorBlue: '#3282F0',
   notoSans: "'Noto Sans KR', sans-serif",
-  flexMixin: (justify = 'center', align = 'center') => `
-  display: flex;
+  flexMixin: (justify = 'center', align = 'center') => `display: flex;
   justify-content: ${justify};
   align-items: ${align}`,
 };
-
 export default theme;
