1436fbcfd228e3b727c04cd90b6f4c2ec1f22e13
somangoi <somi.c.hwang@gmail.com>
2021-07-26 13:09:46 +0900

    e 삭제

--- src/pages/MyPage/History/History.js
+++ src/pages/MyPage/History/History.js
@@ -2,68 +2,125 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import HistoryTable from './HistoryTable';
 import HistoryHeader from './HistoryHeader';
-import PaymentSummaryList from './PaymentSummaryList';
+import Pagination from './Pagination';
+import { HISTORY_API } from '../../../config';
+import { isValidObject } from '../../../utilities/utils';
 
 function History() {
-  const [investLog, setInvestLog] = useState([]);
+  const [investLog, setInvestLog] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState([]);
+  const [currentStatus, setCurrentStatus] = useState(0);
   const [search, setSearch] = useState('');
-  const [filterValue, setFilterValue] = useState('전체');
 
   useEffect(() => {
-    fetch('/data/history.json')
-      .then(res => res.json())
-      .then(res => setInvestLog(res.results));
-  }, []);
+    const LIMIT = 10;
+    const offset = currentPage - 1;
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: localStorage.getItem('accessToken'),
+      },
+    };
 
-  const filterHandler = e => {
-    setFilterValue(e.target.value);
-  };
+    if (currentStatus > 0) {
+      fetch(
+        `${HISTORY_API}?search=${search}&status=${currentStatus}&offset=${
+          offset * LIMIT
+        }&limit=${LIMIT}`,
+        requestOptions
+      )
+        .then(res => res.json())
+        .then(res => {
+          setInvestLog(res);
+          makePageArray(Math.ceil(res.count[currentStatus] / LIMIT));
+        });
+    } else {
+      fetch(
+        `${HISTORY_API}?search=${search}&offset=${
+          offset * LIMIT
+        }&limit=${LIMIT}`,
+        requestOptions
+      )
+        .then(res => res.json())
+        .then(res => {
+          setInvestLog(res);
+          makePageArray(Math.ceil(res.count.all / LIMIT));
+        });
+    }
+  }, [currentPage, currentStatus, search]);
 
-  const handleChange = e => {
+  // 검색기능
+  const searchChange = e => {
     setSearch(e.target.value);
   };
 
-  const filteredList = investLog.filter(product => {
-    return (
-      product.item.includes(search) &&
-      (filterValue !== '전체' ? product.status === filterValue : product.status)
-    );
-  });
+  // 페이지네이션의 버튼 클릭시 페이지 이동을 위한 setState
+  const pageClick = e => {
+    setCurrentPage(e.target.id);
+  };
+
+  // 페이지네이션을 위한 페이지 배열 만들기
+  const makePageArray = pages => {
+    let pageArray = [];
+    for (let i = 1; i <= pages; i++) {
+      pageArray.push(i);
+    }
+    setTotalPage(pageArray);
+  };
+
+  // 투자상태값을 기준으로 필터링하기 위한 setState
+  const filterHandler = e => {
+    setCurrentStatus(e.target.value);
+  };
 
   return (
-    <HistoryWrapper>
-      <div>
-        <SearchBar onChange={handleChange}>
-          <i class="fas fa-search" />
-          <input
-            type="text"
-            placeholder="상품호수 또는 상품명을 검색해보세요"
-          />
-        </SearchBar>
-        <HistoryMain>
-          <HistoryHeader filterHandler={filterHandler} />
-          <PaymentSummary>
-            {paymentList.map((log, idx) => (
-              <PaymentSummaryList
-                filteredList={filteredList}
-                title={log.title}
-                item={log.item}
-                key={idx}
+    <>
+      {isValidObject(investLog) && (
+        <HistoryWrapper>
+          <div>
+            <SearchBar onChange={searchChange}>
+              <i class="fas fa-search" />
+              <input
+                type="text"
+                placeholder="상품호수 또는 상품명을 검색해보세요"
               />
-            ))}
-          </PaymentSummary>
-          <HistoryTable filteredList={filteredList} />
-        </HistoryMain>
-      </div>
-    </HistoryWrapper>
+            </SearchBar>
+            <HistoryMain>
+              <HistoryHeader filterHandler={filterHandler} />
+              <PaymentSummary>
+                {investLog.summary.total > 0
+                  ? Object.entries(investLog.summary)
+                      .map(([name, value]) => [SUMMARY_NAME[name], value])
+                      .map(item => (
+                        <li key={item[0]}>
+                          <div>
+                            <h3>{item[0]}</h3>
+                            <span>{item[1].toLocaleString()}원</span>
+                          </div>
+                        </li>
+                      ))
+                  : '데이터가 존재하지 않습니다.'}
+              </PaymentSummary>
+              <HistoryTable investLogItems={investLog.items} />
+              <Pagination
+                pageClick={pageClick}
+                totalPage={totalPage}
+                currentPage={currentPage}
+              />
+            </HistoryMain>
+          </div>
+        </HistoryWrapper>
+      )}
+    </>
   );
 }
 
-const paymentList = [
-  { title: '투자금액', item: 'invest' },
-  { title: '지급완료 원금', item: 'principal' },
-  { title: '지급완료 이자', item: 'interest' },
-];
+const SUMMARY_NAME = {
+  total: '전체',
+  paidTotal: '지급완료 원금',
+  paidInterest: '지급완료 이자',
+};
 
 const HistoryWrapper = styled.div`
   ${({ theme }) => theme.flexMixin};
---
+++ src/pages/MyPage/History/Pagination.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Pagination({ pageClick, totalPage, currentPage }) {
+  return (
+    <PageWrapper>
+      <PageListBox>
+        <PageBtn>
+          <i className="fas fa-arrow-left"></i>
+        </PageBtn>
+        <Prev>
+          <i className="fas fa-chevron-left"></i>
+        </Prev>
+      </PageListBox>
+      <PageListBox>
+        {totalPage.map(page => (
+          <PageBtn id={page} key={page} onClick={pageClick}>
+            {page}
+          </PageBtn>
+        ))}
+      </PageListBox>
+
+      <PageListBox>
+        <Next>
+          <i className="fas fa-chevron-right"></i>
+        </Next>
+        <PageBtn>
+          <i className="fas fa-arrow-right"></i>
+        </PageBtn>
+      </PageListBox>
+    </PageWrapper>
+  );
+}
+
+const PageWrapper = styled.div`
+  ${({ theme }) => theme.flexMixin('center', 'center')};
+  margin: 20px 0 30px 0;
+`;
+
+const PageListBox = styled.ul`
+  ${({ theme }) => theme.flexMixin('center', 'center')};
+`;
+
+const PageBtn = styled.li`
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-right: none;
+  color: #ccc;
+  text-align: center;
+  cursor: pointer;
+
+  &:last-child {
+    border-right: 1px solid #ccc;
+  }
+
+  &:hover {
+    border: 1px solid black;
+    color: black;
+  }
+`;
+
+const Prev = styled(PageBtn)`
+  margin-right: 40px;
+  border-right: 1px solid #ccc;
+`;
+
+const Next = styled(PageBtn)`
+  margin-left: 40px;
+`;
