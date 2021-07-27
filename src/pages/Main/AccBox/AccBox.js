0631937fecf1664e9d7f126a713c046c5dc36795
kylee <anachrosh@gmail.com>
2021-07-27 12:51:39 +0900

    메인 페이지 누적 정보 완료

--- src/pages/Main/AccBox/AccBox.js
+++ src/pages/Main/AccBox/AccBox.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function AccBox() {
+function AccBox({ today, accInfo }) {
+  const convertFourDigit = price => {
+    return String(price)
+      .split('')
+      .reduceRight((acc, crr, idx, arr) => {
+        const reversedIdx = arr.length - 1 - idx;
+        if (reversedIdx % 4 === 3) {
+          return ',' + crr + acc;
+        }
+        return crr + acc;
+      }, '');
+  };
+
+  const matchKoreanUnit = num => {
+    const unit = ['만원', '억', '조', '경', '해'];
+
+    const arr = num.split(',').filter(el => el !== '');
+    return arr
+      .reduce((acc, crr, idx, arr) => {
+        const reversedIdx = arr.length - 1 - idx;
+        return `${acc} ${Number(crr)}${unit[reversedIdx]}`;
+      }, '')
+      .trim();
+  };
+
   return (
     <AccContainer>
-      <span>2021.07.20 기준</span>
+      <span>{`${today.year}.${today.month}.${today.date} 기준`}</span>
       <AccLoans>
-        <span>누적 대출액</span>
-        <b>3,614억 4,061만원</b>
+        <LoanAccTitle>누적 대출액</LoanAccTitle>
+        <LoanAccContent>
+          {matchKoreanUnit(
+            convertFourDigit(String(accInfo.loanAcc).slice(0, -4))
+          )}
+        </LoanAccContent>
       </AccLoans>
       <AccList>
         <List>
           <span>대출잔액</span>
-          <b>328억 7,747만원</b>
+          <b>328억 7747만원</b>
         </List>
         <List>
           <span>인당 평균 투자금액</span>
-          <b>376만원</b>
+          <b>
+            {matchKoreanUnit(
+              convertFourDigit(String(accInfo.avgPerPerson).slice(0, -4))
+            )}
+          </b>
         </List>
         <List>
           <span>누적 투자건수</span>
-          <b>19,052,632건</b>
+          <b>{accInfo.investAcc.toLocaleString()}건</b>
         </List>
       </AccList>
     </AccContainer>
@@ -33,22 +65,22 @@ const AccContainer = styled.section`
   ${({ theme }) => theme.flexMixin('space-between', 'center')};
   flex-direction: column;
   padding: 50px 0;
-  height: 350px;
+  height: 340px;
 `;
 
 const AccLoans = styled.div`
   ${({ theme }) => theme.flexMixin};
+`;
 
-  span {
-    margin-bottom: -10px;
-    margin-right: 30px;
-    font-size: 24px;
-  }
+const LoanAccTitle = styled.span`
+  margin-bottom: -10px;
+  margin-right: 30px;
+  font-size: 24px;
+`;
 
-  b {
-    font-size: 40px;
-    font-weight: 700;
-  }
+const LoanAccContent = styled.b`
+  font-size: 40px;
+  font-weight: 700;
 `;
 
 const AccList = styled.ul`
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,13 +5,17 @@ import MainSlide from './MainSlide/MainSlide';
 import ProductList from '../../components/ProductList/ProductList';
 import AccBox from './AccBox/AccBox';
 
-import { BASE_URL, MORTGAGE } from '../../config';
 import { SLIDE_BG_COLOR } from './MainSlide/slideBgColor';
+import { BASE_URL, MORTGAGE, LOAN_AMOUNT } from '../../config';
+import { isValidObject } from '../../utilities/utils';
 
 function Main() {
   const [progress, setProgress] = useState(null);
   const [slide, setSlide] = useState(null);
   const [scheduled, setScheduled] = useState(null);
+
+  const [today, setToday] = useState({});
+  const [accInfo, setAccInfo] = useState({});
 
   useEffect(() => {
     fetch(`${BASE_URL}${MORTGAGE}`)
@@ -20,6 +24,27 @@ function Main() {
         setProgress(res.recruitingResults);
         setSlide(res.recruitingResults.slice(0, SLIDE_BG_COLOR.length));
         setScheduled(res.scheduledResults);
+      });
+  }, []);
+
+  useEffect(() => {
+    const newDate = new Date();
+
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth();
+    const date = newDate.getDate();
+
+    setToday({ year, month, date });
+
+    fetch(`${BASE_URL}${LOAN_AMOUNT}`)
+      .then(res => res.json())
+      .then(res => {
+        const { loanAcc, avgPerPerson, investAcc } = res.result;
+        setAccInfo({
+          loanAcc,
+          avgPerPerson,
+          investAcc,
+        });
       });
   }, []);
 
@@ -39,7 +64,9 @@ function Main() {
             오픈 예정 상품
           </ProductList>
         )}
-        <AccBox />
+        {isValidObject(today) && isValidObject(accInfo) && (
+          <AccBox today={today} accInfo={accInfo} />
+        )}
       </Inner>
     </Container>
   );
